3276177886a3531668f31ca015a1af4db8c4ab69
Dharmveer Mahtha <kumardharmveer399@gmail.com>
2022-10-04 13:13:42 +0530

    trado

---
+++ script.js
@@ -0,0 +1,42 @@
+// fns. for copy
+function setClipboard(value) {
+    var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+  
+    var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Mail Copied!";
+  }
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
+
+
+    
+var loader=document.getElementById('loader');
+function myfunc(){
+    loader.style.display='none';
+}
+
+
+function scrollbg(){
+      var navhead= document.getElementById('navhead');
+      var scrollvalue=window.scrollY;
+      var width=window.innerWidth;
+      if(scrollvalue>80){
+        navhead.classList.add("sticky");
+      }
+      else{
+        navhead.classList.remove("sticky");
+      }
+      if(width<1400){
+        navhead.classList.remove("sticky");
+      }
+    // console.log(width);
+}
+window.addEventListener('scroll',scrollbg);
